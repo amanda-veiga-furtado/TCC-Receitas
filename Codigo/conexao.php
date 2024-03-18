8bc559b512e5c52325edbd5bc2efc0d71feed130
Amanda Veiga Furtado <92694622+amanda-veiga-furtado@users.noreply.github.com>
2024-03-18 14:05:51 -0300

    Criação Login

--- Codigo/conexao.php
+++ Codigo/conexao.php
@@ -5,9 +5,15 @@ $pass = "";
 $dbname = "tcc_receitas";
 $port = 3306;
 
-//Conexao com a porta
-$conn = new PDO("mysql:host=$host;port=$port;dbname=".$dbname, $user, $pass);
+try{
+    //Conexao com a porta
+    $conn = new PDO("mysql:host=$host;port=$port;dbname=".$dbname, $user, $pass);
+    echo "<p style='color: green;'>Conexão com banco de dados realizada com sucesso";
 
-//Conexao sem a porta
-//$conn = new PDO("mysql:host=$host;dbname=".$dbname, $user, $pass);
+    //Conexao sem a porta
+    //$conn = new PDO("mysql:host=$host;dbname=".$dbname, $user, $pass);
+} catch(PDOException $err){
+    echo "<p style='color: #f00;'>Erro: Conexão com banco de dados não realizada com sucesso - ". $err->getMessage();
+}
+
 
