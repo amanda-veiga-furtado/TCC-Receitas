169ab100e88718cd0c3caf54279a67b79e009dd2
Amanda Veiga Furtado <92694622+amanda-veiga-furtado@users.noreply.github.com>
2024-03-19 18:16:37 -0300

    Sprint #1

--- Codigo/conexao.php
+++ Codigo/conexao.php
@@ -1,19 +1,20 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "tcc_receitas";
-$port = 3306;
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $dbname = "tcc_receitas";
+    $port = 3306;
 
-try{
-    //Conexao com a porta
-    $conn = new PDO("mysql:host=$host;port=$port;dbname=".$dbname, $user, $pass);
-    echo "<p style='color: green;'>Conexão com banco de dados realizada com sucesso";
+    try{
+        //Conexao com a porta
+            $conn = new PDO("mysql:host=$host;port=$port;dbname=".$dbname, $user, $pass);
+            echo "<p style='color: green;'>Conexão com banco de dados realizada com sucesso";
 
-    //Conexao sem a porta
-    //$conn = new PDO("mysql:host=$host;dbname=".$dbname, $user, $pass);
-} catch(PDOException $err){
-    echo "<p style='color: #f00;'>Erro: Conexão com banco de dados não realizada com sucesso - ". $err->getMessage();
-}
+        //Conexao sem a porta
+            //$conn = new PDO("mysql:host=$host;dbname=".$dbname, $user, $pass);
+            
+    } catch(PDOException $err){
+        echo "<p style='color: #f00;'>Erro: Conexão com banco de dados não realizada com sucesso - ". $err->getMessage();
+    }
 
 
