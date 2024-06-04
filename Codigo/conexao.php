8a5fd5d24b119de4874d1d716ee2fbb242059f7a
Amanda Veiga Furtado <92694622+amanda-veiga-furtado@users.noreply.github.com>
2024-06-04 18:00:44 -0300

    Add files via upload

--- Codigo/conexao.php
+++ Codigo/conexao.php
@@ -8,13 +8,11 @@
     try{
         //Conexao com a porta
             $conn = new PDO("mysql:host=$host;port=$port;dbname=".$dbname, $user, $pass);
-            echo "<p style='color: green;'><br>Conexão com banco de dados realizada com sucesso";
+            echo "<p style='color: green; margin-left: 10px;'><br>Conexão com banco de dados realizada com sucesso";
 
         //Conexao sem a porta
             //$conn = new PDO("mysql:host=$host;dbname=".$dbname, $user, $pass);
             
     } catch(PDOException $err){
-        echo "<p style='color: #f00;'>Erro: Conexão com banco de dados não realizada com sucesso - ". $err->getMessage();
+        echo "<p style='color: #f00; margin-left: 10px;'>Erro: Conexão com banco de dados não realizada com sucesso - ". $err->getMessage();
     }
-
-?>
--- Codigo/menu.php
+++ Codigo/menu.php
@@ -3,15 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Menu</title>
+    <!-- <title>Menu</title> -->
 </head>
 <body>
     <div class="header">
         <ul>
             <li><a class="active" href="#">Home</a></li>
-            <li><a href="#">Livro de Receitas</a></li>
-            <li><a href="TCC\tcc_receitas\usuario\cadastrar.php">Cadastre-se</a></li>
-            <li><a href="TCC\tcc_receitas\usuario\login.php">Logar</a></li>
+            <li><a href="http://localhost/TCC/tcc_receitas/receita/listagem_receita.php">Livro de Receitas</a></li>
+            <li><a href="http://localhost/TCC/tcc_receitas/usuario/cadastrar.php">Cadastre-se</a></li>
+            <li><a href="http://localhost/TCC/tcc_receitas/usuario/login.php">Logar</a></li>
             <li><a href="#">Mais ▼</a></li>
           </ul>
     </div>
